5d3f3d74ce6774e0ac8f3f71eea0ca712c607604
Mariah Morales <mjustine622@gmail.com>
2021-07-07 11:34:59 -0400

    commit tree ee15c2d6400c13824d99ef26cb215d2f28bd9561

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+
+    CLICKBAIT  = [
+        /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+    
+    
+    def is_clickbait?
+        if CLICKBAIT.none? { |t| t.match title }
+          errors.add(:title, "it's clickbait")
+        end
+      end
+    end
